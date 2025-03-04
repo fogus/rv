5d04dd0700c04c8f1d028b30144293e534a8abce
Fogus <mefogus@gmail.com>
2025-03-04 09:36:40 -0500

    wip

--- test/rv/constraint_test.clj
+++ test/rv/constraint_test.clj
@@ -4,31 +4,34 @@
             [fogus.rv.constraints :as c]))
 
 (deftest solve-tests1
-  (let [c1 (c/->constraint [(core/->LVar '?x [0 1])
-                            (core/->LVar '?y [0 1])
-                            (core/->LVar '?z [0 1])]
-                           '(= (+ ?x ?y) ?z))]
-    (is (= [(c/map->cpair '{:domain ?x :value 0})
-            (c/map->cpair '{:domain ?y :value 0})
-            (c/map->cpair '{:domain ?z :value 0})]
+  (let [?x (core/->LVar 'x [0 1])
+        ?y (core/->LVar 'y [0 1])
+        ?z (core/->LVar 'z [0 1])
+        c1 (c/->constraint [?x ?y ?z]
+                           `(= (+ ~?x ~?y) ~?z))]
+    (is (= [(c/map->cpair {:domain ?x :value 0})
+            (c/map->cpair {:domain ?y :value 0})
+            (c/map->cpair {:domain ?z :value 0})]
            (c/find-sat c1)))))
 
 (deftest solve-tests2
-  (let [c1 (c/->constraint [(core/->LVar '?x [0 1])
-                            (core/->LVar '?y [1 2])
-                            (core/->LVar '?z [2 3])]
-                         '(= (+ ?x ?y) ?z))]
-    (is (= [(c/map->cpair '{:domain ?x :value 1})
-            (c/map->cpair '{:domain ?y :value 1})
-            (c/map->cpair '{:domain ?z :value 2})]
+  (let [?x (core/->LVar 'x [0 1])
+        ?y (core/->LVar 'y [1 2])
+        ?z (core/->LVar 'z [2 3])
+        c1 (c/->constraint [?x ?y ?z]
+                           `(= (+ ~?x ~?y) ~?z))]
+    (is (= [(c/map->cpair {:domain ?x :value 1})
+            (c/map->cpair {:domain ?y :value 1})
+            (c/map->cpair {:domain ?z :value 2})]
            (c/find-sat c1)))))
 
 (deftest solve-tests3
-  (let [c1 (c/->constraint [(core/->LVar '?x [1 1])
-                            (core/->LVar '?y [2 2])
-                            (core/->LVar '?z [3 3])]
-                         '(= (+ ?x ?y) ?z))]
-    (is (= [(c/map->cpair '{:domain ?x :value 1})
-            (c/map->cpair '{:domain ?y :value 2})
-            (c/map->cpair '{:domain ?z :value 3})]
+  (let [?x (core/->LVar 'x [1 1])
+        ?y (core/->LVar 'y [2 2])
+        ?z (core/->LVar 'z [3 3])
+        c1 (c/->constraint [?x ?y ?z]
+                           `(= (+ ~?x ~?y) ~?z))]
+    (is (= [(c/map->cpair {:domain ?x :value 1})
+            (c/map->cpair {:domain ?y :value 2})
+            (c/map->cpair {:domain ?z :value 3})]
            (c/find-sat c1)))))
